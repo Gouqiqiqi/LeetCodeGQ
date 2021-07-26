61b13cee24a65d4f55d23cf524ebcae7a5e37c56
gouqiqiqi <867245920@qq.com>
2021-07-26 23:43:53 +0800

    经典题，中序和其他顺序遍历重建二叉树

---
+++ offer/o07/Solution.java
@@ -0,0 +1,27 @@
+package offer.o07;
+
+
+import java.util.Arrays;
+
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if(preorder.length==0) return null;
+        TreeNode nowRoot = new TreeNode(preorder[0]);
+        int spilt = searchIn(preorder[0],inorder);
+        if(spilt!=-1) {
+            nowRoot.left = buildTree(Arrays.copyOfRange(preorder, 1, spilt+1), Arrays.copyOfRange(inorder, 0, spilt ));
+            nowRoot.right = buildTree(Arrays.copyOfRange(preorder, 1 + spilt, preorder.length ), Arrays.copyOfRange(inorder, spilt + 1, inorder.length ));
+        }
+        return  nowRoot;
+    }
+    public int searchIn(int head, int[] in){
+        for(int i = 0 ; i < in.length ; i++){
+            if(in[i]==head) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        new Solution().buildTree(new int[]{3,9,20,15,7},new int[]{9,3,15,20,7});
+    }
+}
