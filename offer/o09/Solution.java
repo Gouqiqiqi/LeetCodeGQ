ee2ae618bd7d5375708cc9107e0b1f20947dfe5e
gouqiqiqi <867245920@qq.com>
2021-07-27 21:57:10 +0800

    双栈建树，熟悉java中栈的结构。评论使用linkedList

---
+++ offer/o09/Solution.java
@@ -0,0 +1,30 @@
+package offer.o09;
+
+import java.util.ArrayDeque;
+
+class CQueue {
+    ArrayDeque<Integer> stack1;
+    ArrayDeque<Integer> stack2;
+    public CQueue() {
+        stack1 = new ArrayDeque<>();
+        stack2 = new ArrayDeque<>();
+    }
+
+    public void appendTail(int value) {
+        while(!stack1.isEmpty()){
+            stack2.push(stack1.pop());
+        }
+        stack1.push(value);
+        while(!stack2.isEmpty()){
+            stack1.push((stack2.pop()));
+        }
+    }
+
+    public int deleteHead() {
+        if(stack1.isEmpty()) return -1;
+        else return stack1.pop();
+    }
+}
+public class Solution {
+
+}
