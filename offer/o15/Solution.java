48429a290614f10671fe47e46df85e7f3a86afac
gouqiqiqi <867245920@qq.com>
2021-08-03 21:58:06 +0800

    位运算思路，取巧32位，不取巧无符号右移>>>

---
+++ offer/o15/Solution.java
@@ -0,0 +1,20 @@
+package offer.o15;
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int res=0;
+        int temp=n;
+//        for(int i = 0;i<32;i++){
+//            if((temp &1)==1) res+=1;
+//            temp=temp>>1;
+//        }
+        while(temp!=0){
+            if((temp &1)==1) res+=1;
+            temp=temp>>>1;
+        }
+        return res;
+    }
+
+
+}
