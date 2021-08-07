0d11c54ff09077aa63cee825c0763458c83871db
gouqiqiqi <867245920@qq.com>
2021-08-07 20:49:27 +0800

    DP练习+贪心练习

---
+++ leet53/Solution.java
@@ -0,0 +1,33 @@
+package leet53;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Solution {
+//    public int maxSubArray(int[] nums) {
+//        int left=0,right = 0;
+//        int sum= 0 ;
+//        int max = Integer.MIN_VALUE;
+//        while(right<nums.length){
+//            sum+=nums[right];
+//            if(sum>max) max=sum;
+//            if(sum<0) sum=0;
+//        }
+//        return max;
+//    }
+    public int maxSubArray(int[] nums) {
+        Integer dp[]= new Integer[nums.length+1];
+        dp[0]=0;
+        int max = Integer.MIN_VALUE;
+        for(int i = 1 ; i < nums.length+1 ; i++){
+            dp[i]= Math.max(dp[i-1]+nums[i-1],nums[i-1]);
+            max= max>dp[i]?max:dp[i];
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        new Solution().maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4});
+    }
+}
