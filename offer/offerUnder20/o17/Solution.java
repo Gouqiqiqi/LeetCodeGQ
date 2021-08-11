6bfdd6cd325dc8796268a87cf968b536a2ae175c
gouqiqiqi <867245920@qq.com>
2021-08-11 16:03:46 +0800

    大数问题，用字符串解决。代码未check

---
+++ offer/offerUnder20/o17/Solution.java
@@ -0,0 +1,27 @@
+package offer.offerUnder20.o17;
+
+import javax.lang.model.type.ArrayType;
+import java.util.ArrayList;
+
+public class Solution {
+    StringBuilder res ;
+    char[] num;
+    public String printNumbers(int n) {
+        res = new StringBuilder();
+        num = new char[n];
+        dfs(n, 0);
+        return res;
+    }
+    public void  dfs(int n,int index){
+        if(index == n ){
+
+            res.append(Integer.parseInt(num.toString()));
+            return;
+        }
+        for(int i = 0 ; i<10; i++){
+            num[index]= (char) ('0'+i);
+            dfs(n,i+1);
+        }
+
+    }
+}
